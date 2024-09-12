5555e4b99af8eb711ca62068e8fefc68915723d1
Le Trung Dung <trungdungle1912@gmail.com>
2024-09-12 16:21:16 +0700

    [Typescript] Basic Types Fix

--- src/type/basic_types/object_type.ts
+++ src/type/basic_types/object_type.ts
@@ -1,4 +1,4 @@
-export const Person: {name: string; age: number, gender: string} = {
+export const Person: {name: string, age: number, gender: string} = {
   name: "Le Trung Dung",
   age: 22,
   gender: "Male"
